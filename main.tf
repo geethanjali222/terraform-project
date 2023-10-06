5adf79c1ce1a6a6333cc948a451a70143f8e957f
geethanjali222 <143397854+geethanjali222@users.noreply.github.com>
2023-10-06 12:01:34 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket369852"
+# main.tf
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "my-first-bucket14752"  
+  
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "my-second-bucket3697"  
+  
+}
+
+# Output the bucket names
+output "bucket1_name" {
+  value = aws_s3_bucket.bucket1.id
+}
+
+output "bucket2_name" {
+  value = aws_s3_bucket.bucket2.id
 }
