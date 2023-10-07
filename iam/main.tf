ef29bfc0008d13109c65e275994b7f61f1ebfa30
geethanjali222 <143397854+geethanjali222@users.noreply.github.com>
2023-10-07 13:09:17 +0530

    Add files via upload

--- iam/main.tf
+++ iam/main.tf
@@ -1,1 +1,15 @@
+resource "aws_iam_user" "iam_user"{
+    name = "terraform_user"
 
+}
+resource "aws_iam_access_key""key"{
+    user = "aws_iam_user.iam_user.name"
+
+}
+output "secret_key"{
+    value = aws_iam_access_key.key.secret
+    sensitive = true
+}
+output "access_key" {
+    value = aws_iam_access_key.key.id 
+}
